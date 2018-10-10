642a2aa7cd4f932590597517416bf2e5019d8935
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2018-10-10 21:49:49 +0300

    Initial

---
+++ build.js
@@ -0,0 +1,33 @@
+
+let rollup = require('rollup')
+let rollupPostcss = require('rollup-plugin-postcss')
+
+let log = console.log
+
+let postcssOptions = {
+	extract: 'dist/styles.css',
+	sourceMap: true,
+	plugins: []
+}
+
+let watchOptions = {
+	input: 'src/main.js',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs'
+	},
+	plugins: [
+		rollupPostcss(postcssOptions)
+	]
+}
+
+let watcher = rollup.watch(watchOptions)
+
+watcher.on('event', event => {
+	console.log('event', event.code)
+	if (event.code === 'BUNDLE_END') {
+		log('Watching files:', event.result.watchFiles)
+	}
+})
+
+
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './main.css'
+
+document.body.innerHTML = '<h1>This is a title</h1><p>Hello world</p>'
